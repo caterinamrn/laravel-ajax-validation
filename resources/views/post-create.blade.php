c5277bea393da2c246cfda332a8f88004687fa46
caterina <catemarian1806@gmail.com>
2020-10-08 16:35:39 +0200

    notifica di errore nel compilare i dati del form

--- resources/views/post-create.blade.php
+++ resources/views/post-create.blade.php
@@ -14,19 +14,34 @@
 
       <div class="form-group">
         <label for="title">title</label>
-        <input type="text" name="title" value="">
+        <input type="text" name="title" class="@error('title') is-invalid @enderror" value="{{old('title')}}">
+        @error('title')
+          <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
       </div>
+
       <div class="form-group">
         <label for="content">content</label>
-        <input type="text" name="content" value="">
+        <input type="text" name="content" class="@error('content') is-invalid @enderror" value="{{old('content')}}">
+        @error('content')
+          <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
       </div>
+
       <div class="form-group">
         <label for="like">like</label>
-        <input type="number" name="like" value="0">
+        <input type="number" name="like" class="@error('like') is-invalid @enderror" value="{{old('like',0)}}">
+        @error('like')
+          <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
       </div>
+
       <div class="form-group">
         <label for="dislike">dislike</label>
-        <input type="number" name="dislike" value="0">
+        <input type="number" name="dislike" class="@error('dislike') is-invalid @enderror" value="{{old('dislike',0)}}">
+        @error('dislike')
+          <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
       </div>
 
       <button class="btn btn-danger" type="submit" name="button">CREATE</button>
